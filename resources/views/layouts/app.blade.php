b10604b2555ebc7d8c34732e4bcf0ce8de673d19
Mazarin <djaiss@users.noreply.github.com>
2024-08-29 16:19:07 -0400

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link rel="preconnect" href="https://fonts.bunny.net" />
     <link href="https://fonts.bunny.net/css?family=inter:400,500,600" rel="stylesheet" />
 
     <!-- Scripts -->
