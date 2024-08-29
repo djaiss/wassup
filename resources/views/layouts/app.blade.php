1f8438929b938276cae925e002af1e6a8c218a75
Mazarin <djaiss@users.noreply.github.com>
2024-08-29 16:18:55 -0400

    chore: add inter font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,8 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net" />
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=inter:400,500,600" rel="stylesheet" />
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -17,7 +17,7 @@
     <!-- Styles -->
     @livewireStyles
   </head>
-  <body class="font-sans antialiased">
+  <body class="antialiased">
     <x-banner />
 
     <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
