0696e3e17da6f4761175c1c17546ee68838f475c
Mazarin <djaiss@users.noreply.github.com>
2024-08-29 13:45:27 -0400

    feat: enable profile picture (#7)

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,5 +1,7 @@
-@props(['for'])
+@props([
+  'for',
+])
 
 @error($for)
-    <p {{ $attributes->merge(['class' => 'text-sm text-red-600 dark:text-red-400']) }}>{{ $message }}</p>
+  <p {{ $attributes->merge(['class' => 'text-sm text-red-600 dark:text-red-400']) }}>{{ $message }}</p>
 @enderror
