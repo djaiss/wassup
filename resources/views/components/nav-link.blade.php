fbfc8b6a236669cd6dad04e5a55a55cd2a08705f
Regis Freyd <djaiss@users.noreply.github.com>
2024-08-31 14:31:11 -0400

    wip

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -9,6 +9,6 @@
       : 'inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium leading-5 text-gray-500 transition duration-150 ease-in-out hover:border-gray-300 hover:text-gray-700 focus:border-gray-300 focus:text-gray-700 focus:outline-none dark:text-gray-400 dark:hover:border-gray-700 dark:hover:text-gray-300 dark:focus:border-gray-700 dark:focus:text-gray-300';
 @endphp
 
-<a {{ $attributes->merge(['class' => $classes]) }}>
+<a wire:navigate {{ $attributes->merge(['class' => $classes]) }}>
   {{ $slot }}
 </a>
