46ea29d13271563234ca338457ef03fed2ae56ab
Regis Freyd <djaiss@users.noreply.github.com>
2024-08-31 09:50:08 -0400

    Update navigation-menu.blade.php

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -12,11 +12,11 @@
 
         <!-- Navigation Links -->
         @if ($organization->id > 0)
-        <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-          <x-nav-link href="{{ route('organizations.index') }}" :active="request()->routeIs('dashboard')">
-            {{ __('Dashboard') }}
-          </x-nav-link>
-        </div>
+          <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+            <x-nav-link href="{{ route('organizations.index') }}" :active="request()->routeIs('dashboard')">
+              {{ __('Dashboard') }}
+            </x-nav-link>
+          </div>
         @endif
       </div>
 
