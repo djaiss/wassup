e5609de0018631bec6a8958d8f26065244c521e8
Regis Freyd <djaiss@users.noreply.github.com>
2024-12-03 06:44:56 -0500

    chore: better goal placeholder

--- app/Livewire/Organizations/Goals/GoalDetails.php
+++ app/Livewire/Organizations/Goals/GoalDetails.php
@@ -52,13 +52,16 @@ class GoalDetails extends Component
     public function placeholder(): string
     {
         return <<<'HTML'
-        <div class="border-b border-gray-200 bg-white mb-4">
+        <div class="border-b border-gray-200 mb-4">
             <div class="flex flex-col space-y-3">
-                <div class="animate-pulse bg-slate-200 h-[125px] w-full rounded-xl"></div>
-                <div class="space-y-2">
-                    <div class="bg-slate-200 animate-pulse rounded-md bg-muted h-4 w-full"></div>
-                    <div class="bg-slate-200 animate-pulse rounded-md bg-muted h-4 w-[200px]"></div>
+                <div class="flex justify-between">
+                    <div class="flex items-center">
+                    <div class="bg-slate-200 animate-pulse rounded-full bg-muted h-4 w-[50px] mr-2"></div>
+                    <div class="bg-slate-200 animate-pulse rounded-md bg-muted h-4 w-[100px]"></div>
+                    </div>
+                    <div class="bg-slate-200 animate-pulse rounded-md bg-muted h-4 w-[100px]"></div>
                 </div>
+                <div class="bg-slate-200 animate-pulse rounded-xl h-[70px] w-full"></div>
             </div>
         </div>
         HTML;
